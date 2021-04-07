fd72c701dff2b6a114b272dba39797a88e531046
liuweiqiu <liuwei_qiu@126.com>
2021-04-07 11:33:32 +0800

    1.第三次更改
    Signed-off-by: liuweiqiu <liuwei_qiu@126.com>

--- main.cpp
+++ main.cpp
@@ -6,6 +6,10 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    //第一次上传数据库
+    //第二次上传数据库
+    //第三次上传数据库
+    //第四次上传数据库
     w.show();
     return a.exec();
 }
